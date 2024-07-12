7d0742a405fea03a3c7600e13f8eae73f6e1f4fd
yCAp1e <2577022217@qq.com>
2024-07-12 21:14:34 +0800

    fix(card:ConstructionRedo): 已修复在使用建筑递归时递归火焰塔、侦测塔时可能发生的错误

--- src/main/java/MoMod/Actions/ConstructionRedoAction.java
+++ src/main/java/MoMod/Actions/ConstructionRedoAction.java
@@ -2,18 +2,19 @@ package MoMod.Actions;
 
 import MoMod.cards.Abstract.AbstractConstructionCard;
 import MoMod.cards.power.constrcution.*;
-import MoMod.power.EMPControlStationPower;
-import MoMod.power.IronCurtainDevicePower;
-import MoMod.power.TacticalNukeSiloPower;
+import MoMod.power.*;
 import MoMod.util.ConstructionPileManager;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.actions.utility.ScryAction;
 import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.powers.AbstractPower;
+import sun.management.Sensor;
 
 public class ConstructionRedoAction extends AbstractGameAction {
     public ConstructionRedoAction() {
@@ -52,6 +53,21 @@ public class ConstructionRedoAction extends AbstractGameAction {
                     this.addToBot(new GainBlockAction(p, lc.block));
                     nc = new InstantShelter(lc.magicNumber);
                 }
+                if (lc instanceof FlameTower) {
+                    AbstractPower po = p.getPower(FlameTowerPower.POWER_ID);
+                    po.flash();
+                    AbstractCreature m = AbstractDungeon.getCurrRoom().monsters.getRandomMonster();
+                    if (m != null && !m.isDead)
+                        this.addToBot(new FireUpAction(m, p, 2));
+                    nc = new FlameTower();
+                }
+                if (lc instanceof SensorTower) {
+                    AbstractPower po = p.getPower(SensorTowerPower.POWER_ID);
+                    po.flash();
+                    this.addToBot(new ScryAction(2));
+                    this.addToBot(new DrawCardAction(p, 1));
+                    nc = new SensorTower();
+                }
                 gp.moveToExhaustPile(lc);
                 this.addToBot(new ApplyPowerAction(p, p, nc.getPower(true)));
                 ConstructionPileManager.buildingConstruction(nc);
@@ -78,7 +94,7 @@ public class ConstructionRedoAction extends AbstractGameAction {
                 if (lc instanceof IronCurtainDevice) {
                     AbstractPower po = p.getPower(IronCurtainDevicePower.POWER_ID);
                     amount = po.amount;
-                    nc = new EMPControlStation();
+                    nc = new IronCurtainDevice(lc.magicNumber);
                     ConstructionPileManager.buildingConstruction(nc);
                     gp.moveToExhaustPile(lc);
                     this.addToBot(new UnitReadyAction(nc.cardsToPreview));
